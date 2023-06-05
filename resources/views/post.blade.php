c1d1834835a268f7d244de84b50d66332fe018aa
Ronal Melgar <melgar1w@gmail.com>
2023-06-05 10:51:34 -0600

    update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -15,64 +15,74 @@
                                 <h5 id="postTitle" class="card-title"></h5>
                                 <p id="postContent" class="card-text"></p>
                                 <img id="postImage" class="img-fluid" alt="">
+                                <p id="postCategory" class="card-text"></p>
+                                <p id="postUser" class="card-text"></p>
                             </div>
                         </div>
                 
                         <h2>Comments</h2>
                         <ul id="commentList" class="list-group"></ul>
                     </div>
-                
-                    <script>
-                        const postId = window.location.pathname.split('/').pop();
-                
-                        axios.get(`http://127.0.0.1:8000/api/posts/${postId}`)
-                            .then(response => {
-                                const post = response.data;
-                
-                                // Actualizar los elementos HTML con los datos del post
-                                const postTitleElement = document.getElementById('postTitle');
-                                postTitleElement.textContent = post.title;
-                
-                                const postContentElement = document.getElementById('postContent');
-                                postContentElement.textContent = post.content;
-                
-                                const postImageElement = document.getElementById('postImage');
-                                postImageElement.src = 'http://127.0.0.1:8000/images/' + post.img;
-                                postImageElement.alt = post.title;
-                            })
-                            .catch(error => {
-                                console.error(error);
-                            });
-                
-                        axios.get(`http://127.0.0.1:8000/api/posts/${postId}/comments`)
-                            .then(response => {
-                                const comments = response.data;
-                
-                                // Obtener el elemento de la lista de comentarios
-                                const commentListElement = document.getElementById('commentList');
-                
-                                // Iterar sobre los comentarios y crear elementos HTML para cada uno
-                                comments.forEach(comment => {
-                                    const commentItem = document.createElement('li');
-                                    commentItem.className = 'list-group-item';
-                                    commentItem.textContent = comment.content;
-                
-                                    commentListElement.appendChild(commentItem);
-                                });
-                            })
-                            .catch(error => {
-                                console.error(error);
-                            });
-                    </script>
-
-
-
-
-
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!-- Agrega el script de Axios -->
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+    const postId = window.location.pathname.split('/').pop();
+
+    // Obtener los detalles del post
+    axios.get(`/api/posts/${postId}`)
+        .then(response => {
+            const post = response.data;
+
+            // Actualizar los elementos HTML con los datos del post
+            const postTitleElement = document.getElementById('postTitle');
+            postTitleElement.textContent = post.title;
+
+            const postContentElement = document.getElementById('postContent');
+            postContentElement.textContent = post.content;
+
+            const postImageElement = document.getElementById('postImage');
+            postImageElement.src = '/images/' + post.img;
+            postImageElement.alt = post.title;
+
+            const postCategoryElement = document.getElementById('postCategory');
+            postCategoryElement.textContent = 'Category: ' + post.category.name;
+
+            const postUserElement = document.getElementById('postUser');
+            postUserElement.textContent = 'Created by: ' + post.user.name;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    // Obtener los comentarios del post
+    axios.get(`/api/posts/${postId}/comments`)
+        .then(response => {
+            const comments = response.data;
+
+            // Obtener el elemento de la lista de comentarios
+            const commentListElement = document.getElementById('commentList');
+
+            // Limpiar la lista de comentarios
+            commentListElement.innerHTML = '';
+
+            // Iterar sobre los comentarios y crear elementos HTML para cada uno
+            comments.forEach(comment => {
+                const commentItem = document.createElement('li');
+                commentItem.className = 'list-group-item';
+                commentItem.textContent = 'Comment: ' + comment.content;
+
+                commentListElement.appendChild(commentItem);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+</script>
+
 @endsection
-<!-- resources/views/posts/show.blade.php -->
--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -1,62 +1,76 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Usuarios</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-      <div class="card">
+@extends('layouts.app')
 
-      
-        <h1 class="mt-5">Usuarios</h1>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Imagen</th>
-                    <th>Fecha de Creación</th>
-                    <th>Fecha de Actualización</th>
-                </tr>
-            </thead>
-            <tbody id="users-table-body">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="container">
+                        <h1>Users</h1>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Image</th>
+                                    <th>Created At</th>
+                                    <th>Updated At</th>
+                                </tr>
+                            </thead>
+                            <tbody id="users-table-body">
+                                <!-- Aquí se agregarán los datos de los usuarios -->
+                            </tbody>
+                        </table>
+                    </div>
                 
-            </tbody>
-        </table>
-      </div>
+                    <!-- Agrega el script de Axios -->
+                    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+                    <script>
+                        // Realiza una solicitud GET a la API y maneja los datos devueltos
+                        axios.get('http://127.0.0.1:8000/api/users')
+                            .then(function (response) {
+                                var users = response.data;
+                                var tableBody = document.getElementById('users-table-body');
+                
+                                // Itera sobre los usuarios y crea filas en la tabla
+                                users.forEach(function (user) {
+                                    var row = document.createElement('tr');
+                
+                                    // Agrega las celdas con los datos del usuario
+                                    row.innerHTML = `
+                                        <td>${user.id}</td>
+                                        <td>${user.name}</td>
+                                        <td>${user.email}</td>
+                                        <td>${user.img ? user.img : 'N/A'}</td>
+                                        <td>${user.created_at}</td>
+                                        <td>${user.updated_at}</td>
+                                    `;
+                
+                                    // Agrega la fila a la tabla
+                                    tableBody.appendChild(row);
+                                });
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    </script>
+
+                    
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+@endsection
 
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script>
-        axios.get('http://127.0.0.1:8000/api/users')
-            .then(function(response) {
-                var users = response.data;
 
-                var tableBody = document.getElementById('users-table-body');
 
-                users.forEach(function(user) {
-                    var row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${user.id}</td>
-                        <td>${user.name}</td>
-                        <td>${user.email}</td>
-                        <td>
-                            <img src="{{ asset('images') }}/${user.img}" alt="Imagen del Usuario" style="width: 100px;">
-                        </td>
-                        <td>${user.created_at}</td>
-                        <td>${user.updated_at}</td>
-                    `;
-
-                    tableBody.appendChild(row);
-                });
-            })
-            .catch(function(error) {
-                console.log(error);
-                // Manejar el error aquí
-            });
-    </script>
-</body>
-</html>
